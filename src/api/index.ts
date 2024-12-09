d53250ba93a99246277f79e68dcfb905541d98fc
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2024-12-09 14:04:14 +0530

    changes in the api endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL =
+  'https://face-api-backend-3wf86n0wc-palakk2098s-projects.vercel.app/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
