c08d1f31b623f87161c733493d50f5e660171d5c
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2024-12-10 14:33:07 +0530

    update changes for api endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,16 @@ const App: React.FC = () => {
   return (
     <div style={{ textAlign: 'center', margin: '2rem' }}>
       <ToastContainer position="top-right" autoClose={3000} />
-
+      <h1>Face Recognition App</h1>
       <img
         src="face-recognition-logo.jpeg"
         alt="face-app-logo"
         className="rounded-image"
       />
 
-      <h1>Face Recognition App</h1>
       <AddFace />
+      <br />
+      <hr />
       <RecognizeFace />
     </div>
   );
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  'https://face-api-backend-3wf86n0wc-palakk2098s-projects.vercel.app/api';
+  'https://face-api-backend-xjaw-cqo0ww42o-palakk2098s-projects.vercel.app/api';
+// const API_BASE_URL = 'http://localhost:8080/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
